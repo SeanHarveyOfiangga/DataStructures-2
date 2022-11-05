9d7e9a09d1a4ebee87544d4646f3a67ad54c3fb1
unknown <seanharveyofiangga@gmail.com>
2022-11-05 13:28:23 +0800

    Created a for loop function that ask the user for 10 random numbers

---
+++ seatwork.py
@@ -0,0 +1,48 @@
+# Write a python program that do the following:
+
+# - display the initial content of the array
+# - display a menu of options
+# - allow user to select item in the menu (check if valid)
+# - perform the selected option (you may prompt additional info to user when need)
+# - display the resulting values of the array
+
+
+# Note: 
+# - The program has an array variable containing 10 random numbers
+# - You may add other options and features
+# - Your program should be uploaded to github before 1:30pm
+# - Record a demo presenting your program
+# - Send the demo directly to my messenger
+
+# Example Output:
+
+# Array: [1, 4, 3, 4, 4, 5, 6 ,2 ,56, 200]
+# Menu:
+#  1 -> Add an element
+#  2 -> Insert an element
+#  3 -> Modify an element
+#  4 -> Delete an element
+#  5 -> Arrange in ascending order
+#  6 -> Arrange in descending order
+# What do you want to do? (1-6): 4
+# Enter the index you want to delete: 3
+# The element has been deleted
+# This is the new array: Array: [1, 4, 3, 4, 5, 6 ,2 ,56, 200]
+
+#Introduction and user input
+
+def Intro():
+    print("Welcome to my array program!")
+Intro()
+#User input
+
+def User():
+    list1 = []
+    for i in range(10):
+        content = int(input("\nPlease enter 10 random numbers: "))
+        list1.append(content)
+        print(list1)
+    print(f"\nYou picked {list1}")
+    return list1
+User()
+    
